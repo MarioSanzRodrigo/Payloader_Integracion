ad7f0060301abf4361cb904c15c21887bef2996e
MarioSanzRodrigo <m.sanz741@gmail.com>
2019-02-06 10:11:58 +0100

    ficheros .h modificados para MediaProcessors

--- payloader/src/logger.h
+++ payloader/src/logger.h
@@ -1,3 +1,7 @@
+/*################################################
+######### INTEGRACION PAYLOADER UPM ##############
+#################################################*/
+
 /**
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
